3783530cf19a543af5f63efffffce2fd3e9ca320
adriankiprono <tuimuradrian6@gmail.com>
2019-09-19 10:57:58 +0300

    add division

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,11 @@ var multiplication = function(number1, number2) {
 var number1= parseInt(prompt( "Enter a number(multiplication):"));
 var number2= parseInt(prompt("Enter another number:"));
 alert(multiplication(number1,number2));
+var result =add(number1, number2)
+alert(result);
+var divide= function(number1, number2) {
+  return number1 / number2;
+};
+var number1= parseInt(prompt( "Enter a number(divide):"));
+var number2= parseInt(prompt("Enter another number:"));
+alert(divide(number1,number2));
